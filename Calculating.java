6e4976c6c9d0c7411b1656e3c139b9c92a45af99
lucpro9001 <lucpro9001@gmail.com>
2021-09-25 20:31:15 +0700

    bai tap thuc hanh 2

---
+++ Calculating.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculating;
+
+import java.awt.*;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+/**
+ *
+ * @author Admin
+ */
+public class Calculating {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Main();
+
+    }
+
+}
